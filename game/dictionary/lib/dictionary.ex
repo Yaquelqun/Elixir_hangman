ae573760f3a098e33bd106d9756b5773cbe2048f
Yaquelqun <sandjiv.parassou@gmail.com>
2020-03-26 15:01:45 +0100

    make start link work

--- game/dictionary/lib/dictionary.ex
+++ game/dictionary/lib/dictionary.ex
@@ -1,6 +1,6 @@
 defmodule Dictionary do
   alias Dictionary.Generator
 
-  defdelegate start(), to: Generator, as: word_list()
-  defdelegate random_word(words), to: Generator
+  defdelegate random_word(agent), to: Generator
+  defdelegate start(), to: Generator, as: :start_link
 end
--- game/dictionary/lib/dictionary/generator.ex
+++ game/dictionary/lib/dictionary/generator.ex
@@ -1,6 +1,14 @@
 
 defmodule Dictionary.Generator do
-  
+
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
+  end
+
   def word_list() do
     '../../assets/words.txt'
     |> Path.expand(__DIR__)
@@ -8,8 +16,5 @@ defmodule Dictionary.Generator do
     |> String.split(~r/\n/)
   end
 
-  def random_word(words) do
-    words
-    |> Enum.random
-  end
+
 end
