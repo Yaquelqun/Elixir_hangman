5d2e13ae4ec5aacb06e2813867545b2c827bf8f5
Yaquelqun <sandjiv.parassou@gmail.com>
2020-03-26 16:05:56 +0100

    dictionary is now a standalone application

--- game/dictionary/lib/dictionary.ex
+++ game/dictionary/lib/dictionary.ex
@@ -1,6 +1,5 @@
 defmodule Dictionary do
   alias Dictionary.Generator
 
-  defdelegate random_word(agent), to: Generator
-  defdelegate start(), to: Generator, as: :start_link
+  defdelegate random_word(), to: Generator
 end
--- game/dictionary/lib/dictionary/generator.ex
+++ game/dictionary/lib/dictionary/generator.ex
@@ -1,12 +1,17 @@
 
 defmodule Dictionary.Generator do
 
+  @me __MODULE__
+
   def start_link() do
-    Agent.start_link(&word_list/0)
+    Agent.start_link(&word_list/0, name: @me)
   end
 
-  def random_word(agent) do
-    Agent.get(agent, &Enum.random/1)
+  def random_word() do
+    if :rand.uniform < 0.33 do
+      Agent.get(@me, fn _-> exit(:boom) end)
+    end
+    Agent.get(@me, &Enum.random/1)
   end
 
   def word_list() do
